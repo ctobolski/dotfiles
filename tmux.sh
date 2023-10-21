07db9e2886880d92a4584f5a16cbd8616eb352f2
Chris Tobolski <chris@canopy.security>
2023-10-21 12:19:07 -0400

    tmux config

---
+++ tmux.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# add to path in zshrc
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+echo "Don't forget to add to path in zshrc"
