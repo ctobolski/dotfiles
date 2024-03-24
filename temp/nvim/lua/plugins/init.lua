c826d4593f8ee8b50461b3c71bb99de842559fab
Chris Tobolski <chris@canopy.security>
2024-03-24 18:56:53 -0400

    current nvim config for work

---
+++ temp/nvim/lua/plugins/init.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+    {
+      "LazyVim",
+      opts = {
+        colorscheme = "gruvbox",
+      },
+    },
+  },
+
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "json5",
+        "jsonc",
+      },
+    },
+  },
+}
